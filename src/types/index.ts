a6c814580e4df0c6fe04697509e22e9626e1587d
Waswa Olunga <olungaw@gmail.com>
2025-12-07 23:09:53 -0800

    feat: removes unnecessary type files

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,12 @@
-export * from "./timer";
+export type TimerConfig = {
+  id: string;
+  initialDuration: number;
+  title: string;
+};
+
+export type TimerType = {
+  id: string;
+  numIterations: number;
+  timerConfigs: TimerConfig[];
+  title: string;
+};
