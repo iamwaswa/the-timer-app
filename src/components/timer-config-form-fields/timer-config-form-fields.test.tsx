251c5fa1cb707fc163ba0f07658e6985497bc3df
Waswa Olunga <olungaw@gmail.com>
2025-12-15 09:28:44 -0800

    feat: adds component folder tests

---
+++ src/components/timer-config-form-fields/timer-config-form-fields.test.tsx
@@ -0,0 +1,128 @@
+import { act, render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+
+import { generateRandomUUID } from "@/utils";
+
+import { TimerConfigFormFields } from "./timer-config-form-fields";
+
+vi.mock("@/utils", () => ({
+  ...vi.importActual("@/utils"),
+  generateRandomUUID: vi.fn(),
+}));
+
+const generateRandomUUIDMock = vi.mocked(generateRandomUUID);
+
+beforeEach(() => {
+  vi.resetAllMocks();
+});
+
+it("should render the form with initial values", () => {
+  const timerConfigs = [
+    { id: "1", title: "Timer 1", initialDuration: 10 },
+    { id: "2", title: "Timer 2", initialDuration: 20 },
+  ];
+
+  render(<TimerConfigFormFields timerConfigs={timerConfigs} updateTimerConfigs={vi.fn()} />);
+
+  expect(screen.getAllByRole("textbox", { name: /Title/ })).toHaveLength(2);
+  expect(screen.getAllByRole("spinbutton", { name: /Initial Duration \(seconds\)/ })).toHaveLength(2);
+});
+
+it("should call updateTimerConfigs when adding a new timer", async () => {
+  const event = userEvent.setup();
+  const newTimerUuid = "new-uuid";
+  generateRandomUUIDMock.mockReturnValue(newTimerUuid);
+  const timerConfigs = [{ id: "1", title: "Timer 1", initialDuration: 10 }];
+  const updateTimerConfigs = vi.fn();
+
+  render(<TimerConfigFormFields timerConfigs={timerConfigs} updateTimerConfigs={updateTimerConfigs} />);
+
+  await act(() => event.click(screen.getByRole("button", { name: "Add Timer" })));
+
+  expect(updateTimerConfigs).toHaveBeenCalledWith([
+    ...timerConfigs,
+    { id: newTimerUuid, title: "New interval", initialDuration: 60 },
+  ]);
+});
+
+it("should call updateTimerConfigs when updating a timer title", async () => {
+  const event = userEvent.setup();
+
+  let timerConfigs = [
+    { id: "1", title: "Timer 1", initialDuration: 10 },
+    { id: "2", title: "Timer 2", initialDuration: 20 },
+  ];
+  const updateTimerConfigs = vi.fn().mockImplementation((newTimerConfigs) => {
+    timerConfigs = newTimerConfigs;
+  });
+
+  const { rerender } = render(
+    <TimerConfigFormFields timerConfigs={timerConfigs} updateTimerConfigs={updateTimerConfigs} />,
+  );
+
+  await act(() => event.clear(screen.getAllByRole("textbox", { name: /Title/ })[0]));
+
+  expect(updateTimerConfigs).toHaveBeenLastCalledWith([{ ...timerConfigs[0], title: "" }, timerConfigs[1]]);
+
+  rerender(<TimerConfigFormFields timerConfigs={timerConfigs} updateTimerConfigs={updateTimerConfigs} />);
+
+  const nextCharacter = "s";
+  await act(() => event.type(screen.getAllByRole("textbox", { name: /Title/ })[0], nextCharacter));
+
+  expect(updateTimerConfigs).toHaveBeenLastCalledWith([{ ...timerConfigs[0], title: nextCharacter }, timerConfigs[1]]);
+});
+
+it("should call updateTimerConfigs when updating a timer duration", async () => {
+  const event = userEvent.setup();
+  let timerConfigs = [
+    { id: "1", title: "Timer 1", initialDuration: 10 },
+    { id: "2", title: "Timer 2", initialDuration: 20 },
+  ];
+  const updateTimerConfigs = vi.fn().mockImplementation((newTimerConfigs) => {
+    timerConfigs = newTimerConfigs;
+  });
+
+  const { rerender } = render(
+    <TimerConfigFormFields timerConfigs={timerConfigs} updateTimerConfigs={updateTimerConfigs} />,
+  );
+
+  await act(() => event.clear(screen.getAllByRole("spinbutton", { name: /Initial Duration \(seconds\)/ })[0]));
+
+  expect(updateTimerConfigs).toHaveBeenLastCalledWith([{ ...timerConfigs[0], initialDuration: 0 }, timerConfigs[1]]);
+
+  rerender(<TimerConfigFormFields timerConfigs={timerConfigs} updateTimerConfigs={updateTimerConfigs} />);
+
+  const nextCharacter = "1";
+  await act(() =>
+    event.type(screen.getAllByRole("spinbutton", { name: /Initial Duration \(seconds\)/ })[0], nextCharacter),
+  );
+
+  expect(updateTimerConfigs).toHaveBeenLastCalledWith([
+    { ...timerConfigs[0], initialDuration: Number(nextCharacter) },
+    timerConfigs[1],
+  ]);
+});
+
+it("should call updateTimerConfigs when deleting a timer", async () => {
+  const event = userEvent.setup();
+  const timerConfigs = [
+    { id: "1", title: "Timer 1", initialDuration: 10 },
+    { id: "2", title: "Timer 2", initialDuration: 20 },
+  ];
+  const updateTimerConfigs = vi.fn();
+
+  render(<TimerConfigFormFields timerConfigs={timerConfigs} updateTimerConfigs={updateTimerConfigs} />);
+
+  await act(() => event.click(screen.getAllByRole("button", { name: "Delete timer" })[0]));
+
+  expect(updateTimerConfigs).toHaveBeenCalledWith([timerConfigs[1]]);
+});
+
+it("should disable the delete button when there is only one timer", () => {
+  const timerConfigs = [{ id: "1", title: "Timer 1", initialDuration: 10 }];
+  const updateTimerConfigs = vi.fn();
+
+  render(<TimerConfigFormFields timerConfigs={timerConfigs} updateTimerConfigs={updateTimerConfigs} />);
+
+  expect(screen.getByRole("button", { name: "Delete timer" })).toBeDisabled();
+});
---
+++ src/components/timer-form/timer-form.test.tsx
@@ -0,0 +1,155 @@
+import { act, render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import { redirect } from "next/navigation";
+
+import { useTimerForm } from "@/hooks";
+
+import { TimerForm } from "./timer-form";
+
+vi.mock("@/hooks", () => ({
+  ...vi.importActual("@/hooks"),
+  useTimerForm: vi.fn(),
+}));
+
+const useTimerFormMock = vi.mocked(useTimerForm);
+
+vi.mock("next/navigation", () => ({
+  ...vi.importActual("next/navigation"),
+  redirect: vi.fn(),
+}));
+
+const redirectMock = vi.mocked(redirect);
+
+beforeEach(() => {
+  vi.resetAllMocks();
+});
+
+it("should render the form with initial values", () => {
+  const timerForm = {
+    id: "1",
+    numIterations: 3,
+    timerConfigs: [],
+    title: "Test Timer",
+  };
+  const timerFormActions = {
+    save: vi.fn(),
+    updateTitle: vi.fn(),
+    updateNumIterations: vi.fn(),
+    updateTimerConfigs: vi.fn(),
+  };
+  useTimerFormMock.mockImplementation(() => [timerForm, timerFormActions]);
+
+  render(<TimerForm />);
+
+  expect(screen.getByRole("textbox", { name: /Title/ })).toHaveValue(timerForm.title);
+  expect(screen.getByRole("spinbutton", { name: /Number of iterations/ })).toHaveValue(timerForm.numIterations);
+  expect(screen.getByRole("button", { name: "Create Timer" })).toBeInTheDocument();
+});
+
+it("should call updateTitle when the title is changed", async () => {
+  const event = userEvent.setup();
+  const timerForm = {
+    id: "1",
+    numIterations: 3,
+    timerConfigs: [],
+    title: "Test Timer",
+  };
+  const timerFormActions = {
+    save: vi.fn(),
+    updateTitle: vi.fn().mockImplementation((newTitle) => {
+      timerForm.title = newTitle;
+    }),
+    updateNumIterations: vi.fn(),
+    updateTimerConfigs: vi.fn(),
+  };
+  useTimerFormMock.mockImplementation(() => [timerForm, timerFormActions]);
+
+  const { rerender } = render(<TimerForm />);
+
+  await act(() => event.clear(screen.getByRole("textbox", { name: /Title/ })));
+
+  expect(timerFormActions.updateTitle).toHaveBeenLastCalledWith("");
+
+  rerender(<TimerForm />);
+
+  const nextCharacter = "s";
+  await act(() => event.type(screen.getByRole("textbox", { name: /Title/ }), nextCharacter));
+
+  expect(timerFormActions.updateTitle).toHaveBeenLastCalledWith(nextCharacter);
+});
+
+it("should call updateNumIterations when the number of iterations is changed", async () => {
+  const event = userEvent.setup();
+  const timerForm = {
+    id: "1",
+    numIterations: 3,
+    timerConfigs: [],
+    title: "Test Timer",
+  };
+  const timerFormActions = {
+    save: vi.fn(),
+    updateTitle: vi.fn(),
+    updateNumIterations: vi.fn().mockImplementation((newNumIterations) => {
+      timerForm.numIterations = newNumIterations;
+    }),
+    updateTimerConfigs: vi.fn(),
+  };
+  useTimerFormMock.mockImplementation(() => [timerForm, timerFormActions]);
+
+  const { rerender } = render(<TimerForm />);
+
+  await act(() => event.clear(screen.getByRole("spinbutton", { name: /Number of iterations/ })));
+
+  expect(timerFormActions.updateNumIterations).toHaveBeenLastCalledWith(0);
+
+  rerender(<TimerForm />);
+
+  const nextCharacter = "1";
+  await act(() => event.type(screen.getByRole("spinbutton", { name: /Number of iterations/ }), nextCharacter));
+
+  expect(timerFormActions.updateNumIterations).toHaveBeenLastCalledWith(Number(nextCharacter));
+});
+
+it("should call save and redirect on form submission", async () => {
+  const event = userEvent.setup();
+  const timerForm = {
+    id: "1",
+    numIterations: 3,
+    timerConfigs: [],
+    title: "Test Timer",
+  };
+  const timerFormActions = {
+    save: vi.fn(),
+    updateTitle: vi.fn(),
+    updateNumIterations: vi.fn(),
+    updateTimerConfigs: vi.fn(),
+  };
+  useTimerFormMock.mockImplementation(() => [timerForm, timerFormActions]);
+
+  render(<TimerForm />);
+
+  await act(() => event.click(screen.getByRole("button", { name: "Create Timer" })));
+
+  expect(timerFormActions.save).toHaveBeenCalled();
+  expect(redirectMock).toHaveBeenCalledWith("/");
+});
+
+it('should display "Update Timer" when a timer prop is provided', () => {
+  const timerForm = {
+    id: "1",
+    numIterations: 2,
+    timerConfigs: [],
+    title: "Existing Timer",
+  };
+  const timerFormActions = {
+    save: vi.fn(),
+    updateTitle: vi.fn(),
+    updateNumIterations: vi.fn(),
+    updateTimerConfigs: vi.fn(),
+  };
+  useTimerFormMock.mockImplementation(() => [timerForm, timerFormActions]);
+
+  render(<TimerForm timer={timerForm} />);
+
+  expect(screen.getByRole("button", { name: "Update Timer" })).toBeInTheDocument();
+});
